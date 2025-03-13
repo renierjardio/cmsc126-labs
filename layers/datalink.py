d1b522ddb64ce8f6808f9da969bd2d1239d35e3a
Renier Jardio <renierjardio@gmail.com>
2025-03-13 09:09:15 +0800

    added MAC address function

--- layers/datalink.py
+++ layers/datalink.py
@@ -1,13 +1,13 @@
 import json
+import uuid
 
 class DataLinkLayer:
-    def __init__(self, mac_address):
-        self.mac_address = mac_address
-        
-    def send(self, data):
-        frame = {'MAC': self.mac_address, 'Payload': data}
-        print(f"[DATA_LINK] Encapsulating frame: {frame}")
-        return json.dumps(frame)
+    def __init__(self):
+        self.mac_address = self.get_mac_address()
+
+    def get_mac_address(self):
+        mac = ':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff) for elements in range(0, 2 * 6, 8)][::-1])
+        return mac
     
     def receive(self, frame):
         frame_data = json.loads(frame)
