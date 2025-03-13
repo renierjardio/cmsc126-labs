40f33bff3c7a506e1095fe8accf07898e6c57490
Renier Jardio <renierjardio@gmail.com>
2025-03-13 10:34:20 +0800

    fixed send function

--- layers/datalink.py
+++ layers/datalink.py
@@ -9,6 +9,11 @@ class DataLinkLayer:
         mac = ':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff) for elements in range(0, 2 * 6, 8)][::-1])
         return mac
     
+    def send(self, data):
+        frame = {'MAC': self.mac_address, 'Payload': data}
+        print(f"[DATA_LINK] Encapsulating frame: {frame}")
+        return json.dumps(frame)
+    
     def receive(self, frame):
         frame_data = json.loads(frame)
         print(f"[DATA_LINK] Decapsulating frame: {frame_data}")
