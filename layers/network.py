ffc5d11e1c1a27d258f180bd786c95c27632d318
Renier Jardio <renierjardio@gmail.com>
2025-03-13 09:10:33 +0800

    added IP address function

--- layers/network.py
+++ layers/network.py
@@ -1,8 +1,19 @@
 import json
+import socket
 
 class NetworkLayer:
-    def __init__(self, ip_address):
-        self.ip_address = ip_address
+    def __init__(self):
+        self.ip_address = self.get_ip_address()
+
+    def get_ip_address(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("8.8.8.8", 80))
+            ip = s.getsockname()[0]
+            s.close()
+            return ip
+        except Exception as e:
+            return "127.0.0.1"
         
     def send(self, data, dest_ip):
         packet = {'IP': self.ip_address, 'Dest_IP': dest_ip, 'Data': data}
