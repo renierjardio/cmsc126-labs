1029909284f6d0a894f0bef4391c7a7df64867a4
Renier Jardio <renierjardio@gmail.com>
2025-03-16 09:21:29 +0800

    fixed MAC address retrieval

--- layers/datalink.py
+++ layers/datalink.py
@@ -6,8 +6,9 @@ class DataLinkLayer:
         self.mac_address = self.get_mac_address()
 
     def get_mac_address(self):
-        mac = ':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff) for elements in range(0, 2 * 6, 8)][::-1])
-        return mac
+        mac = uuid.getnode()
+        mac_address = ':'.join(f'{(mac >> i) & 0xff:02x}' for i in reversed(range(0, 48, 8)))
+        return mac_address
     
     def send(self, data):
         frame = {'MAC': self.mac_address, 'Payload': data}
